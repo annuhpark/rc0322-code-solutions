b84f43ab2732e7effd22de19ac2db6d59d791d18
Anna Park <parkannuh@gmail.com>
2022-03-21 17:16:10 +0000

    Completed javascript-primitives-and-variables Exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,17 @@
+var fullName = 'Anna Park';
+var isCool = false;
+var totalPets = 0;
+var unknown = null;
+var never;
+
+console.log('Full Name:', fullName);
+console.log('Am I Cool?', isCool);
+console.log('Total Number of Pets:', totalPets);
+
+console.log('Type of fullName:', typeof fullName);
+console.log('Type of isCool:', typeof isCool);
+console.log('Type of totalPets:', typeof totalPets);
+console.log('Unknown', unknown);
+console.log('Type of unknown:', typeof unknown);
+console.log('Never', never);
+console.log('Type of never:', typeof never);
