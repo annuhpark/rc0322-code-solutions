56cff1f9d28b22917217940ca8bb04fb2224b2ff
Anna Park <parkannuh@gmail.com>
2022-03-28 17:24:41 +0000

    Completed dom-querying Exercise.

---
+++ dom-querying/main.js
@@ -0,0 +1,14 @@
+var $h1 = document.querySelector('h1');
+console.dir($h1);
+
+var $explanation = document.querySelector('#explanation');
+console.dir($explanation);
+
+var $hint = document.querySelector('.hint');
+console.dir($hint);
+
+var $p = document.querySelectorAll('p');
+console.log($p);
+
+var $exampleLink = document.querySelectorAll('.example-link');
+console.log($exampleLink);
