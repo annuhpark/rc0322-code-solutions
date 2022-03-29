e73224329b1bb55bfb6a01452e5f5b44f00a36ee
Anna Park <parkannuh@gmail.com>
2022-03-29 04:36:41 +0000

    Completed object-manipulation-2 Exercise.

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -2,8 +2,9 @@
 // Direction reworded: There are two inputted objects, the first one being the one that needs to be modified. The property and values from the second object should be added to the first. Nothing is returned from this function. If the first object already has the same property name, even with a different value from the second object, then first object keeps its original property and value.
 // 1. Create a for in loop that loops through the first object.
 // 2. Follow for in loop from step 1 with another for in loop that loops through the second object.
-// 3. Create an if statement with the condition that if the property already exists, to continue with the for loop without any additions.
-// 4. Add the property and values onto the first object otherwise.
+// 3. Create an if statement with the condition that if the property already exists, to delete the property from the "source" object.
+// 4. Follow with an else if statement saying that if the property from the "source" object is not part of the "target" object, to add it into the target object.
+// 5. Hard code with an if statement at the beginning saying that if the target object is an empty object, make the target object equal to the source object.
 
 function defaults(target, source) {
   if (Object.keys(target).length === 0) {
