3365f8c27252ccbe6bc75b6136034e52b6f386b9
Anna Park <parkannuh@gmail.com>
2022-04-01 07:18:38 +0000

    Completed array-manipulation-3 Exercise.

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,4 +1,8 @@
 /* exported unique */
+// Direction reworded: Only include unique elements in the returned array, aka cannot repeat same elements.
+// 1. Use the set and new method as well as spread operator to create a new array that only has unique elements.
+// 2. Return the new array from the function.
+
 function unique(array) {
   var uniqueArray = [...new Set(array)];
   return uniqueArray;
