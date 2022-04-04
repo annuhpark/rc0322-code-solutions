9c68a4b3024f5a87ffdb96d3fba1018253db353f
Anna Park <parkannuh@gmail.com>
2022-04-04 17:45:30 +0000

    In Progress.

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,1 +1,58 @@
 /* exported isAnagram */
+
+function isAnagram(firstString, secondString) {
+  var firstStringToArray = firstString.split('').sort().join('');
+  var secondStringToArray = secondString.split('').sort().join('');
+  // console.log(firstStringToArray, secondStringToArray);
+  // console.log(firstStringToArray === secondStringToArray);
+  if (firstStringToArray === secondStringToArray) {
+    return true;
+  } else if (firstStringToArray.indexOf(' ') !== -1 && secondStringToArray.indexOf(' ') !== -1) {
+    firstStringToArray = firstString.split('').sort();
+    for (let i = 0; i < firstStringToArray.length; i++) {
+      if (firstStringToArray[i] === ' ') {
+        firstStringToArray.splice(i, 1);
+        firstStringToArray = firstStringToArray.join('');
+        // console.log(firstStringToArray);
+      }
+    }
+    secondStringToArray = secondString.split('').sort();
+    for (let y = 0; y < secondStringToArray.length; y++) {
+      if (secondStringToArray[y] === ' ') {
+        secondStringToArray.splice(y, 1);
+        secondStringToArray = secondStringToArray.join('');
+        // console.log(secondStringToArray);
+      }
+    }
+    if (secondStringToArray === firstStringToArray) {
+      return true;
+    } else {
+      return false;
+    }
+  } else if (firstStringToArray !== secondStringToArray && firstStringToArray.indexOf(' ') === -1 && secondStringToArray.indexOf(' ') === -1) {
+    return false;
+  }
+  if (firstStringToArray.indexOf(' ') === -1 && secondStringToArray.indexOf(' ') !== -1) {
+    secondStringToArray = secondStringToArray.split('').sort();
+    var newArray = [];
+    // console.log(secondStringToArray);
+    for (let y = 0; y < secondStringToArray.length; y++) {
+      if (secondStringToArray[y] === ' ') {
+        continue;
+      } else {
+        newArray.push(secondStringToArray[y]);
+        // console.log(newArray);
+      }
+    }
+    newArray = newArray.join('');
+    // console.log(newArray);
+    // console.log(firstStringToArray);
+    // console.log(secondStringToArray);
+    // console.log(firstStringToArray);
+    if (newArray === firstStringToArray) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
--- string-manipulation-3/src/is-palindromic.js
+++ string-manipulation-3/src/is-palindromic.js
@@ -1,21 +1,33 @@
 /* exported isPalindromic */
 // Direction reworded: Check if the inputted string is a palindrome. Return a boolean.
-// 1. Change the inputted string into an array using the split(); method.
-// 2. Use the reverse method to flip the letters.
-// 3. Use the join method to change back into a string and set the return of the method as a variable.
-// 4. Use an if statement to compare the old and new string.
 
 function isPalindromic(string) {
-  var stringToArray = string.split('').reverse();
-  var arrayToString = stringToArray.join('');
-  for (let i = 0; i < stringToArray.length; i++) {
-    if (stringToArray[i] === ' ') {
-      arrayToString = stringToArray.splice(i, 1).join('');
+  var newArray = [];
+  // console.log(arrayToString);
+  // console.log(stringToArray);
+  // console.log(string);
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === ' ') {
+      // console.log('there is a space');
+      // console.log(stringToArray);
+      continue;
+    } else {
+      newArray.push(string[i]);
+      // console.log(newArray);
     }
   }
-  if (string === arrayToString) {
-    return true;
-  } else {
-    return false;
+  var copiedArray = [...newArray];
+  var reversedArray = copiedArray.reverse();
+  // console.log(reversedArray);
+  var arrayToString = reversedArray.join('');
+  // console.log(newArray);
+  // console.log(arrayToString);
+  for (let i = 0; i < newArray.length; i++) {
+    if (newArray[i] === arrayToString[i]) {
+      continue;
+    } else {
+      return false;
+    }
   }
+  return true;
 }
