3df84e678b2d1b6ac1a6f99b99987d05227c793a
Anna Park <parkannuh@gmail.com>
2022-04-13 20:04:23 +0000

    Edited interval and updated dots at bottom with each interval loop.

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,17 +1,18 @@
 var $image = document.querySelector('img');
 var imageSrcValues = ['025.png', '004.png', '007.png', '039.png', '001.png'];
 var indexOfImage = 0;
+var intervalIndex = 0;
 var $buttonCircles = document.querySelector('.circle-buttons-container');
 var $circles = document.querySelectorAll('.fa-circle');
 var $leftArrow = document.querySelector('.fa-angle-left');
 var $rightArrow = document.querySelector('.fa-angle-right');
 
 function changeImage() {
-  // console.log(indexOfImage);
-  $image.setAttribute('src', 'images/' + imageSrcValues[indexOfImage]);
-  indexOfImage++;
-  if (indexOfImage === 5) {
-    indexOfImage = 0;
+  $image.setAttribute('src', 'images/' + imageSrcValues[intervalIndex]);
+  matchIntervalToCircle();
+  intervalIndex++;
+  if (intervalIndex === 5) {
+    intervalIndex = 0;
   }
 }
 
@@ -23,43 +24,55 @@ $buttonCircles.addEventListener('click', function (event) {
       $circles[i].className = 'fas fa-circle';
       $image.setAttribute('src', 'images/' + imageSrcValues[i]);
       indexOfImage = i;
-      // console.log(indexOfImage);
+      intervalIndex = 0;
     } else {
       $circles[i].className = 'far fa-circle';
+      intervalIndex = 0;
     }
   }
 });
 
 $leftArrow.addEventListener('click', function (event) {
-  // console.log(indexOfImage);
   if (indexOfImage > 0 && indexOfImage <= 4) {
     $image.setAttribute('src', 'images/' + imageSrcValues[indexOfImage - 1]);
-    // console.log(indexOfImage);
     indexOfImage = indexOfImage - 1;
     matchArrowToCircle();
+    intervalIndex = 0;
   } else if (indexOfImage <= 0) {
     $image.setAttribute('src', 'images/' + imageSrcValues[4]);
     indexOfImage = 4;
     matchArrowToCircle();
+    intervalIndex = 0;
   }
 });
 
 $rightArrow.addEventListener('click', function (event) {
   if (indexOfImage >= 0 && indexOfImage < 4) {
     $image.setAttribute('src', 'images/' + imageSrcValues[indexOfImage + 1]);
-    // console.log(indexOfImage);
     indexOfImage = indexOfImage + 1;
     matchArrowToCircle();
+    intervalIndex = 0;
   } else if (indexOfImage >= 4) {
     $image.setAttribute('src', 'images/' + imageSrcValues[0]);
     indexOfImage = 0;
     matchArrowToCircle();
+    intervalIndex = 0;
   }
 });
 
 function matchArrowToCircle() {
   for (let i = 0; i < $circles.length; i++) {
     if (i === indexOfImage) {
+      $circles[i].className = 'fas fa-circle';
+    } else {
+      $circles[i].className = 'far fa-circle';
+    }
+  }
+}
+
+function matchIntervalToCircle() {
+  for (let i = 0; i < $circles.length; i++) {
+    if (i === intervalIndex) {
       $circles[i].className = 'fas fa-circle';
     } else {
       $circles[i].className = 'far fa-circle';
