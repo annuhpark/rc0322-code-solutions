5ed68f64c36b7973fd0acbd5edd92585fb5c83b8
Anna Park <parkannuh@gmail.com>
2022-05-15 00:58:05 +0000

    In Progress.

---
+++ sgt-back-end/index.js
@@ -0,0 +1,48 @@
+const pg = require('pg');
+const express = require('express');
+const app = express();
+const db = new pg.Pool({
+  connectionString: 'postgres://dev:dev@localhost/studentGradeTable',
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+app.use(express.json());
+
+app.get('/api/grades', (req, res) => {
+  const sql = `
+    select "gradeId",
+           "name",
+           "course",
+           "score",
+           "createdAt"
+      from "public"."grades"
+  `;
+  db.query(sql)
+    .then(result => {
+      const grade = result.rows;
+      res.status(200).json(grade);
+      // console.log(result.rows);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occurred.'
+      });
+    });
+});
+
+app.post('/api/grades', (req, res) => {
+  const newGrade = {
+    name: req.body.name,
+    course: req.body.course,
+    score: req.body.score
+  };
+  // console.log(newGrade);
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Express server listening on port 3000');
+});
