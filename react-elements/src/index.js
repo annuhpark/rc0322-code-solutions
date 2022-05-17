f7a7b47260ba3754b87296f7d24cf5140c655238
Anna Park <parkannuh@gmail.com>
2022-05-17 23:19:58 +0000

    Completed react-elements Exercise.

---
+++ react-elements/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom/client';
+const text = React.createElement(
+  'h1',
+  null,
+  'Hello, React!'
+);
+
+const $container = document.getElementById('root');
+const root = ReactDOM.createRoot($container);
+root.render(text);
