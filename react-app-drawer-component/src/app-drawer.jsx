26c5e2d5b50d4ce04acc2669f3e8840771c832f4
Anna Park <parkannuh@gmail.com>
2022-05-23 22:00:21 +0000

    Completed react-app-drawer-component Exercise.

---
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+export default class Drawer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.showDrawer = this.showDrawer.bind(this);
+    this.hideDrawer = this.hideDrawer.bind(this);
+    this.state = { burgerClicked: false };
+  }
+
+  showDrawer(event) {
+    if (event.target.className === 'fas fa-bars') {
+      this.setState({ burgerClicked: true });
+    }
+  }
+
+  hideDrawer(event) {
+    if (event.target.className.includes('link') || (event.target.className.includes('page-container'))) {
+      this.setState({ burgerClicked: false });
+    }
+  }
+
+  render() {
+    const burgerClass = 'fas fa-bars';
+    let menuClass;
+    let pageClass;
+    if (this.state.burgerClicked === true) {
+      menuClass = 'drawer white-background-color drawer-reveal';
+      pageClass = 'page-container full-vp transparent-background-color';
+    } else if (this.state.burgerClicked === false) {
+      menuClass = 'drawer white-background-color';
+      pageClass = 'page-container full-vp';
+    }
+    return (
+      <div className='background-pepe full-vp'>
+        <div onClick={this.hideDrawer} className={pageClass}>
+          <i onClick={this.showDrawer} className={burgerClass}></i>
+          <div className={menuClass}>
+            <div>
+              <h1>Menu</h1>
+            </div>
+            <div className="align-items-center link">
+              <a className="target-link">About</a>
+            </div>
+            <div className="align-items-center link">
+              <a className="target-link">Get Started</a>
+            </div>
+            <div className="align-items-center link">
+              <a className="target-link">Sign In</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
