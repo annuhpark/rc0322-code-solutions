34090d365854140ac0964f852726e921c04d34c2
Anna Park <parkannuh@gmail.com>
2022-05-24 22:50:17 +0000

    Completed fetch-in-react Exercise.

---
+++ fetch-in-react/src/app.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import UserList from './user-list';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      isLoading: true
+    };
+  }
+
+  componentDidMount() {
+    /* your code here */
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => {
+        // console.log(data);
+        this.setState({
+          users: data,
+          isLoading: false
+        });
+        // console.log(this.state.users);
+      });
+  }
+
+  render() {
+    return this.state.isLoading
+      ? <p>Loading...</p>
+      : <UserList users={this.state.users} />;
+  }
+}
+
+export default App;
